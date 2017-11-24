16715a35b33d4d484cb78128f19bc79a959d67a5
IoanaB <ioana.bacrau@gmail.com.com>
2017-11-24 00:38:14 +0000

    doamne ajuta

--- app.js
+++ app.js
@@ -58,16 +58,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
                 session.send(messages.food);
          //       var msg = new builder.Message(session).addAttachment(foodcard);
                 var attachment = session.message.attachments[0];
-                session.send({
-                    text: "You sent:",
-                    attachments: [
-                        {
-                            contentType: attachment.contentType,
-                            contentUrl: attachment.contentUrl,
-                            name: attachment.name
-                        }
-                    ]
-                });
+                session.send(foodcard);
                 break;
                 }
             case 'cake':
